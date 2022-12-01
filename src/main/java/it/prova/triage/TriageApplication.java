fdfd972409074d3c1ce9772352a9dc3274264a70
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-01 16:53:59 +0100

    first commit

---
+++ src/main/java/it/prova/triage/TriageApplication.java
@@ -0,0 +1,13 @@
+package it.prova.triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriageApplication.class, args);
+	}
+
+}
