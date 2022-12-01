3ce043f01fa8d6f4ebd3aeffc3f5a4974af8d3b1
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-01 17:26:21 +0100

    Implemented UtenteDTO and Paziente DTO

--- src/main/java/it/prova/triage/dto/PazienteDTO.java
+++ src/main/java/it/prova/triage/dto/PazienteDTO.java
@@ -1,5 +1,91 @@
 package it.prova.triage.dto;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+
+import it.prova.triage.model.StatoPaziente;
+
 public class PazienteDTO {
+
+	private Long id;
+
+	@NotBlank(message = "{nome.notblank}")
+	private String nome;
+
+	@NotBlank(message = "{cognome.notblank}")
+	private String cognome;
+
+	@NotBlank(message = "{codicefiscale.notblank}")
+	private String codiceFiscale;
+
+	private LocalDate dataRegistrazione;
+
+	private StatoPaziente stato;
+
+	public PazienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PazienteDTO(Long id, String nome, String cognome, String codiceFiscale, LocalDate dataRegistrazione,
+			StatoPaziente stato) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public LocalDate getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(LocalDate dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoPaziente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoPaziente stato) {
+		this.stato = stato;
+	}
+	
+	
 
 }
--- src/main/java/it/prova/triage/dto/UtenteDTO.java
+++ src/main/java/it/prova/triage/dto/UtenteDTO.java
@@ -1,5 +1,117 @@
 package it.prova.triage.dto;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import it.prova.triage.model.StatoUtente;
+
 public class UtenteDTO {
+
+	private Long id;
+
+	@NotBlank(message = "{username.notblank}")
+	@Size(min = 3, max = 15, message = "Il valore inserito '${validatedValue}' deve essere lungo tra {min} e {max} caratteri")
+	private String username;
+
+	@NotBlank(message = "{password.notblank}")
+	@Size(min = 8, max = 15, message = "Il valore inserito deve essere lungo tra {min} e {max} caratteri")
+	private String password;
+
+	private String confermaPassword;
+
+	@NotBlank(message = "{nome.notblank}")
+	private String nome;
+
+	@NotBlank(message = "{cognome.notblank}")
+	private String cognome;
+
+	private LocalDate dataRegistrazione;
+
+	private StatoUtente stato;
+
+	public UtenteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UtenteDTO(Long id, String username, String password,String confermaPassword, String nome, String cognome,
+			LocalDate dataRegistrazione, StatoUtente stato) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.confermaPassword = confermaPassword;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfermaPassword() {
+		return confermaPassword;
+	}
+
+	public void setConfermaPassword(String confermaPassword) {
+		this.confermaPassword = confermaPassword;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public LocalDate getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(LocalDate dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoUtente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoUtente stato) {
+		this.stato = stato;
+	}
+	
+	
 
 }
